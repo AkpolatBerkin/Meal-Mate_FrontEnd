36cee3e8413695326fdafead0808d6ca91eb07f2
AkpolatBerkin <berkinakpolat@gmail.com>
2025-05-25 16:59:17 +0200

    Überarbeitung

--- Meal-Mate/src/components/__tests__/HelloWorld.spec.ts
+++ Meal-Mate/src/components/__tests__/HelloWorld.spec.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import RezeptListe from '../RezeptListe.vue'
+import RezeptListe from '../Rezeptliste.vue'
 
-describe('RezeptListe', () => {
+describe('Rezeptliste', () => {
   it('wird korrekt gerendert', () => {
     const wrapper = mount(RezeptListe)
     expect(wrapper.exists()).toBe(true)
